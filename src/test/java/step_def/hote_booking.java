6d7d906492dc96a64785a060d4419c816e23aebf
janasagar <ancrea007@gmail.com>
2024-07-08 16:39:28 +0530

    First commit

---
+++ src/test/java/step_def/hote_booking.java
@@ -0,0 +1,118 @@
+package step_def;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class hote_booking {
+	
+	WebDriver driver;
+	
+	@Given("User visited to mackmytrip website")
+	public void user_visited_to_mackmytrip_website() throws InterruptedException {
+	    // Write code here that turns the phrase above into concrete actions
+		driver = new ChromeDriver();
+		driver.get("https://www.makemytrip.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(4000);
+	    
+	}
+
+	@When("User select hotel")
+	public void user_select_hotel() throws InterruptedException {
+		driver.findElement(By.xpath("(//span[@class='headerIconWrapper'])[2]")).click();
+		Thread.sleep(4000);
+	    
+	}
+
+	@When("User Enter location, date, rooms, guests, price per night")
+	public void user_enter_location_date_rooms_guests_price_per_night() throws InterruptedException {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.findElement(By.xpath("//label[@for='city']")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//input[@placeholder='Where do you want to stay?']")).sendKeys("Munnar");
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("(//b[contains(text(),'Munnar')])[2]")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//*[@id=\"top-banner\"]/div[2]/div/div[1]/div[2]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[2]/div[1]/div[3]/div[5]/div[1]")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//*[@id=\"top-banner\"]/div[2]/div/div[1]/div[2]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[2]/div[1]/div[3]/div[5]/div[4]")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//button[normalize-space()='Apply']")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.id("appliedFilter")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//li[contains(text(),'₹5000+')]")).click();
+		Thread.sleep(3000);
+		
+	    
+	}
+
+	@When("User click on search button")
+	public void user_click_on_search_button() throws InterruptedException {
+		
+	    // Write code here that turns the phrase above into concrete actions
+		driver.findElement(By.id("hsw_search_button")).click();
+		
+	    
+	}
+
+	@Then("Hotels should be displayed")
+	public void hotels_should_be_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("User list the hotel names")
+	public void user_list_the_hotel_names() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("User filter the result with breakfast included, rating, room view")
+	public void user_filter_the_result_with_breakfast_included_rating_room_view() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("User select Blanket Hotel & spa Munnar")
+	public void user_select_blanket_hotel_spa_munnar() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("User Swicth to child tab")
+	public void user_swicth_to_child_tab() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("User click on Book now button")
+	public void user_click_on_book_now_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("User enter Guest details")
+	public void user_enter_guest_details() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("User click on pay now button")
+	public void user_click_on_pay_now_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("User Should redirect to chackout page")
+	public void user_should_redirect_to_chackout_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+}
---
+++ target/test-classes/makemytrip/book_hotel.feature
@@ -0,0 +1,18 @@
+@tag
+Feature: Makemytrip hotel booking
+
+  @tag1
+  Scenario: Search and book book a hotel
+    Given User visited to mackmytrip website
+    When User select hotel
+    And User Enter location, date, rooms, guests, price per night  
+    And User click on search button 
+    Then Hotels should be displayed
+    And User list the hotel names
+    And User filter the result with breakfast included, rating, room view
+    And User select Blanket Hotel & spa Munnar
+    And User Swicth to child tab
+    And User click on Book now button
+    And User enter Guest details
+    And User click on pay now button
+    Then User Should redirect to chackout page
